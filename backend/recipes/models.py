633b491f4c6618d19d71250c33b33f79e1ed264d
Заборовский Денис <zrivkoren@gmail.com>
2022-10-02 12:54:22 +0800

    добавлена модель тега

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -3,19 +3,37 @@ from users.models import User
 
 
 class Ingredient(models.Model):
-    name = models.CharField(
-        'Наименование ингредиента',
-        max_length=200
-    )
-    measurement_unit = models.CharField(
-        'Единица измерения',
-        max_length=200
-    )
+    name = models.CharField('Наименование ингредиента', max_length=200)
+    measurement_unit = models.CharField('Единица измерения', max_length=200)
 
     class Meta:
-        ordering = ['name',]
+        ordering = ['name', ]
         verbose_name = 'Ингредиент'
 
     def __str__(self):
         return self.name
 
+
+class Tag(models.Model):
+    name = models.CharField(
+        'Название тега',
+        max_length=200,
+        unique=True
+    )
+    color = models.CharField(
+        'Цвет в теге',
+        max_length=7,
+        unique=True
+    )
+    slug = models.SlugField(
+        'Уникальный слаг',
+        max_length=200,
+        unique=True
+    )
+
+    class Meta:
+        ordering =['name',]
+        verbose_name = 'Тег'
+
+    def __str__(self):
+        return self.name

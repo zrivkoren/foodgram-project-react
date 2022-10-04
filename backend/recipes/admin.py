b94459e45ce175903bf910ba535e07224187218c
Заборовский Денис <zrivkoren@gmail.com>
2022-10-04 09:25:25 +0800

    добавлен вывод рецептов, тегов с ингредиентами в админку

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredient, Recipe, Tag, IngredientsInRecipe
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit',)
+    search_fields = ('name',)
+
+@admin.register(Tag)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug',)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('author', 'name', 'text',)
+    list_filter = ('author',)
+
+@admin.register(IngredientsInRecipe)
+class IngredientsInRecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'ingredient', 'amount',)
+
